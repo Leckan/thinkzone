ab7986f7e2362d14fe2357aa7ff641f4a598aa10
Leckan <ahmed2real@gmail.com>
2025-02-28 01:54:50 -0800

    sentry client user feedback

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Clients from "@/components/Clients";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/floating-navbar";
@@ -5,19 +6,27 @@ import Grid from "@/components/ui/Grid";
 import { ModeToggle } from "@/components/ui/ModeToggle";
 import { navItems } from "@/data";
 import Image from "next/image";
-import { FaAndroid, FaBriefcase, FaFile, FaHome, FaInfo, FaPeopleArrows, FaPhone, FaProjectDiagram } from "react-icons/fa";
-
+import {
+  FaAndroid,
+  FaBriefcase,
+  FaFile,
+  FaHome,
+  FaInfo,
+  FaPeopleArrows,
+  FaPhone,
+  FaProjectDiagram,
+} from "react-icons/fa";
 
 export default function Home() {
   return (
-   <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-    <div className="max-w-7xl w-full">
-      <FloatingNav navItems={navItems}/>
-      <Hero/>
-      <Grid/>
-      <RecentProjects/>
-     
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
       </div>
-   </main>
+    </main>
   );
 }
